68f602bb63233ab4f822583a09b5e4afd383e283
Suraj Kumar <118210509+Sg-suraj@users.noreply.github.com>
2026-01-25 22:23:39 +0530

    Day_9 Update

---
+++ Day_9/Basic_String1.java
@@ -0,0 +1,26 @@
+package Day_9;
+
+public class Basic_String1{
+    public static void main(String[] args){
+        String a = "Suraj Kumar";
+
+        //checking the length of the String //
+        System.out.println(a.length());
+
+        //checking the char at specific lenght//
+        System.out.println(a.charAt(6));
+
+        //to lower case //
+        System.out.println(a.toLowerCase());
+
+        //to upper case //
+        System.out.println(a.toUpperCase());
+
+        //trimming the string //
+        System.out.println(a.trim());
+
+        //substring //
+        System.out.println(a.substring(0,5));
+
+    }
+}
