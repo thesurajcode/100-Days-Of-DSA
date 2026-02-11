ae45ea0d228c43c3f0259ca57dd6bdd10a150400
Suraj Kumar <118210509+Sg-suraj@users.noreply.github.com>
2026-02-12 01:37:50 +0530

    Updating Day_18

---
+++ Day_18/Inbuilt.java
@@ -0,0 +1,15 @@
+package Day_18;
+
+public class Inbuilt {
+    public static void main(String[] args){
+        String Text = "thesurajcode";
+        String Pattern = "code";
+
+        if(Text.indexOf(Pattern) != -1){
+            System.out.println("Found");
+        }else{
+            System.out.println("Not Found");
+        }
+    }
+    
+}
