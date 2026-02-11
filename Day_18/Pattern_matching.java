a6f2581c26167bb014e7a99976874dc64b56f4e5
Suraj Kumar <118210509+Sg-suraj@users.noreply.github.com>
2026-02-12 00:33:41 +0530

    Updating Day_18

---
+++ Day_18/Pattern_matching.java
@@ -0,0 +1,32 @@
+package Day_18;
+
+public class Pattern_matching {
+    public static void main(String[] args){
+        String Text = "codeLeuetLeet";
+        String Pattern = "Leet";
+
+        int m = Text.length();
+        int n = Pattern.length();
+        boolean found = false;;
+
+
+        for(int i = 0; i <= m-n; i++){
+            int j;
+            for(j = 0; j < n; j++){
+                if(Text.charAt(i+j) !=  Pattern.charAt(j)){
+                    break;
+                }
+            }
+            if(j == n){
+                found = true;
+                break;
+
+            }
+        }
+        if(found){
+            System.out.println("Pattern is Found");
+        }else{
+            System.out.println("Pattern is Not found ");
+        }
+    }
+}
